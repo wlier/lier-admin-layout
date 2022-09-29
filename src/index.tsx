814da4a23ec4b454b2af35dc3d4f255ccf60d1bb
lier <2390197194@qq.com>
2022-09-29 19:08:53 +0800

    feat:可拖动体验

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './pages/App'
 import './index.less'
 import 'antd/dist/antd.variable.min.css'
+import 'react-dragula/dist/dragula.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(<App />)
--- src/pages/efficiency/project-manager/ProjectManager.tsx
+++ src/pages/efficiency/project-manager/ProjectManager.tsx
@@ -1,7 +1,88 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './ProjectManager.less'
+import dragula from 'react-dragula'
+import { Col, Row } from 'antd'
 
 const ProjectManager: React.FC = () => {
-  return <div className='ProBox'>project manager</div>
+  // dragula([document.getElementById('left'), document.getElementById('right')], {
+  //   direction: 'vertical',
+  //   moves: (el: any, container: any, handle: any) => {
+  //     return handle.classList.contains('handle')
+  //   },
+  // })
+  // dragula([document.getElementById('left')], { direction: 'vertical' })
+  // dragula([document.getElementById('right')], { direction: 'vertical' })
+
+  useEffect(() => {
+    dragula(
+      [document.getElementById('left'), document.getElementById('right')],
+      {
+        accepts: function (el: any, target: any) {
+          console.log('el', el)
+          console.log('target', target)
+          return target === document.getElementById('right')
+        },
+      }
+    )
+  }, [])
+  return (
+    <div className='ProBox'>
+      <Row>
+        <Col span={6}>
+          <div id='left'>
+            <div style={{ background: 'blue', height: 20 }}></div>
+            <div style={{ background: 'green', height: 20 }}></div>
+            <div style={{ background: 'pink', height: 20 }}></div>
+            <div style={{ background: 'pink', height: 20 }}>
+              Swap him around
+            </div>
+            <div style={{ background: 'pink', height: 20 }}>
+              Swap them around
+            </div>
+            <div style={{ background: 'pink', height: 20 }}>Swap us around</div>
+            <div style={{ background: 'pink', height: 20 }}>
+              Swap things around
+            </div>
+            <div style={{ background: 'pink', height: 20 }}>
+              Swap everything around
+            </div>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div id='right' style={{ background: 'pink' }}>
+            <div style={{ background: 'red', height: 20 }}>西瓜</div>
+            <div style={{ background: 'yellow', height: 20 }}>苹果</div>
+            <div style={{ background: 'pink', height: 20 }}>香蕉</div>
+            <div style={{ background: 'blue', height: 20 }}>葡萄</div>
+            <div style={{ background: 'blue', height: 20 }}>榴莲</div>
+            <div style={{ background: 'blue', height: 20 }}>橘子</div>
+            <div style={{ background: 'blue', height: 20 }}>石榴</div>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div id='right1' style={{ background: 'pink' }}>
+            <div style={{ background: 'red', height: 20 }}>西瓜1</div>
+            <div style={{ background: 'yellow', height: 20 }}>苹果1</div>
+            <div style={{ background: 'pink', height: 20 }}>香蕉1</div>
+            <div style={{ background: 'blue', height: 20 }}>葡萄1</div>
+            <div style={{ background: 'blue', height: 20 }}>榴莲1</div>
+            <div style={{ background: 'blue', height: 20 }}>橘子1</div>
+            <div style={{ background: 'blue', height: 20 }}>石榴1</div>
+          </div>
+        </Col>
+        <Col span={6}>
+          <div id='right2' style={{ background: 'pink' }}>
+            <div style={{ background: 'red', height: 20 }}>西瓜2</div>
+            <div style={{ background: 'yellow', height: 20 }}>苹果2</div>
+            <div style={{ background: 'pink', height: 20 }}>香蕉2</div>
+            <div style={{ background: 'blue', height: 20 }}>葡萄2</div>
+            <div style={{ background: 'blue', height: 20 }}>榴莲2</div>
+            <div style={{ background: 'blue', height: 20 }}>橘子2</div>
+            <div style={{ background: 'blue', height: 20 }}>石榴2</div>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  )
 }
 export default ProjectManager
